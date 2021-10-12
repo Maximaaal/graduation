1f9e14f65a800f4d6f1c1c527ef5beae1e27848c
Maximaaal <maxrentmeester@live.nl>
2021-10-12 12:42:37 +0200

    domain + ip

--- dns/dns.py
+++ dns/dns.py
@@ -1,47 +1,46 @@
 import time
 import os
 import re
+import json
+import socket
 
 
 def follow(thefile):
-    '''generator function that yields new lines in a file
-    '''
-    # seek the end of the file
+
     thefile.seek(0, os.SEEK_END)
-    
-    # start infinite loop
+
     while True:
-        # read last line of file
         line = thefile.readline()
-        # sleep if file hasn't been updated
         if not line:
             time.sleep(0.1)
             continue
 
         yield line
 
+class colors:
+    YELLOW = '\033[93m'
+    ENDC = '\033[0m'
+
 if __name__ == '__main__':
     
     logfile = open("/var/log/pihole.log","r")
     loglines = follow(logfile)
-    # iterate over the generator
-    
-    # for line in loglines:
-    #     line = line.split(":",1)
-    #     print(line, end='')
-
-    
-     
-    # initializing the list object
-    lst=[]
-    
-    #lst.append(pattern.search(line)[0])
-    #print(lst, end='')
     
     for line in loglines:
         pattern = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', line)
-        del pattern[0]
-        print(pattern)
+        pattern.remove('192.168.0.176')
+        string = "reply"
+        pattern = re.split(' ', line)
+        print(pattern[7])
+        
+        try:
+            print(colors.YELLOW + socket.gethostbyname(pattern[7]) + colors.ENDC)
+        except socket.error:
+            pass
+        
+
+        
+        
         
         
         
